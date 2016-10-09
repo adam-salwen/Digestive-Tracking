b29f15bf323c407e8adece5170d6b018debd3078
Adam <abs265@cornell.edu>
2016-10-09 16:55:43 -0400

    enabled SSL with WEBrick

--- app/assets/javascripts/records/form.js
+++ app/assets/javascripts/records/form.js
@@ -1,11 +1,37 @@
 $(document).ready(function() {
     $("body").append("<p> Testing Testing 123 </p>");
-    
+    $('p#pooVolume').hide()
+    $('p#foodMedIn').hide();
+    $('p#labName').hide();
+    $('p#labVal').hide();
 
-    $('button').click(function() {
-	var test=$('#record_rType_1').val();
+    $('input[name="record[rType]"]').click(function() {
+	$('p#pooVolume').hide()
+	$('p#foodMedIn').hide();
+	$('p#labName').hide();
+	$('p#labVal').hide();
 
-	$('body').append('<p>' + test + '</p>');
+	var rType=$('input[name="record[rType]"]:checked').val();
+	$('body').append('<p>' + rType + '</p>');
+	$('body').append('<p>' + 'test' + rType +'test' +  '</p>');
+	switch(parseInt(rType, 10) ) {
+	case 1:
+	    $('p#pooVolume').fadeIn('fast');
+	    break;
+	case 3:
+	    $('p#labName').fadeIn('fast');
+	    $('p#labVal').fadeIn('fast');
+	    break
+	case 4:
+	    $('p#foodMedIn').fadeIn('fast');
+	    break;
+	    
+	default:
+	    $('body').append('<p>' + 'now' + '</p>');
+	
+	    break;
+	};
+
 
     });
 
